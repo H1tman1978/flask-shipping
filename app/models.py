8cf3fe5b233d0948bfbd9dc59424e953fb506506
nelson-david <larva.code15@gmail.com>
2020-10-30 11:59:59 -0700

    re-created register models and register form

--- app/models.py
+++ app/models.py
@@ -12,11 +12,11 @@ class Users(UserMixin, db.Model):
 	last_name = db.Column(db.String(70), nullable=False)
 	user_name = db.Column(db.String(70), nullable=False)
 	email = db.Column(db.String(100), nullable=False)
-	slack_handle: db.Column(db.String(100), nullable=False)
+	slack_handle = db.Column(db.String(100), nullable=False)
 	password = db.Column(db.String(100), nullable=False)
 	date_joined = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
 
-	# shippings = db.relationship("Shipping", backref="author", lazy=True)
+	shippings = db.relationship("Shipping", backref="author", lazy=True)
 
 	def __repr__(self):
 		return f"Name: {self.name} Email: {self.email} Date Joined: {self.date_joined} "
@@ -25,29 +25,32 @@ class Users(UserMixin, db.Model):
 class Shipping(db.Model):
 	id = db.Column(db.Integer, primary_key=True, unique=True)
 	# Addresses
-	attention: db.Column(db.String(70), nullable=False)
-	company: db.Column(db.String(70), nullable=False)
-	address1: db.Column(db.String(70), nullable=False)
-	address2: db.Column(db.String(70), nullable=False)
-	city: db.Column(db.String(70), nullable=False)
-	state_province: db.Column(db.String(70), nullable=True)
-	postal_code: db.Column(db.String(70), nullable=False)
-	country: db.Column(db.String(70), nullable=True)
-	template_name: db.Column(db.String(70), nullable=True)
+	attention = db.Column(db.String(70), nullable=False)
+	company = db.Column(db.String(70), nullable=False)
+	address1 = db.Column(db.String(70), nullable=False)
+	address2 = db.Column(db.String(70), nullable=False)
+	city = db.Column(db.String(70), nullable=False)
+	state_province = db.Column(db.String(70), nullable=True)
+	postal_code = db.Column(db.String(70), nullable=False)
+	country = db.Column(db.String(70), nullable=True)
+	template_name = db.Column(db.String(70), nullable=True)
 
-	handling_unit = db.relationship("HandlingUnit", backref="shipping_handling_unit", lazy=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+	handling_unit = db.relationship("HandlingUnit", backref="shipping_handling_unit",\
+		lazy=True)
 
 class HandlingUnit(db.Model):
 	# HANDLING_UNIT:
-	case_number: db.Column(db.Integer, primary_key=True, unique=True)
+	case_number = db.Column(db.Integer, primary_key=True, unique=True)
+
+	type = db.Column(db.String(70), nullable=False) #string field (drop down field with box, pallet, case, drum, crate as options) Required
+	length = db.Column(db.Integer(), nullable=True) #float number representing the length of the handling unit. Optional for creating instruction, mandatory for shipping
+	width = db.Column(db.Integer(), nullable=True) #same as length
+	height = db.Column(db.Integer(), nullable=True) #same as length
+
+	weight = db.Column(db.Integer(), nullable=False) #(weights will be rounded up to the nearest whole unit.) Optional for creating instruction, mandatory for shipping
+	has_shipped = db.Column(db.Boolean(), nullable=False)
+	tracking_number = db.Column(db.String(70), nullable=False)
+	carrier = db.Column(db.String(70), nullable=False)
+
 	instruction_id = db.Column(db.Integer, db.ForeignKey('shipping.id'), nullable=False)
-
-	type: db.Column(db.String(70), nullable=False) #string field (drop down field with box, pallet, case, drum, crate as options) Required
-	length: db.Column(db.Integer(), nullable=True) #float number representing the length of the handling unit. Optional for creating instruction, mandatory for shipping
-	width: db.Column(db.Integer(), nullable=True) #same as length
-	height: db.Column(db.Integer(), nullable=True) #same as length
-
-	weight: db.Column(db.Integer(), nullable=False) #(weights will be rounded up to the nearest whole unit.) Optional for creating instruction, mandatory for shipping
-	has_shipped: db.Column(db.Boolean(), nullable=False)
-	tracking_number: db.Column(db.String(70), nullable=False)
-	carrier: db.Column(db.String(70), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,11 @@ def register():
 		user = Users.query.filter_by(email=request.form["email"]).first()
 		if user:
 			return jsonify({'data':'exist'})
-		new_user = Users(name=request.form["name"], email=request.form["email"],\
+		new_user = Users(first_name=request.form["first_name"],\
+			last_name=request.form["last_name"],\
+			email=request.form["email"],\
+			user_name = request.form["username"],
+			slack_handle = request.form["slack_handle"],\
 			password=hash_password(request.form["password"]))
 		db.session.add(new_user)
 		db.session.commit()
@@ -55,18 +59,9 @@ def shipping():
 	if current_user.is_authenticated:
 		if request.method == "POST":
 			data = request.form
-			new_shipping = Shipping(ship_to_company=data['ship_to_company'],\
-				ship_from_company=data["ship_from_company"], ship_to_city=data["ship_to_city"],\
-				ship_from_city=data["ship_from_city"], ship_to_zip=data["ship_to_zip"],\
-				ship_from_zip=data["ship_from_zip"], ship_to_phone_number=data["ship_to_phone_number"],\
-				external_phone_number=data["external_phone_number"], ship_to_address=data["ship_to_address"],
-				ship_from_address=data["ship_from_address"], ship_to_state=data["ship_to_state"],\
-				ship_from_state=data["ship_from_state"], senders_name=data["senders_name"],\
-				special_instructions=data["special_instructions"],\
-				department=data["department"], item_description=data["item_description"],\
-				quantity=data["quantity"], author=current_user)
 
-			db.session.add(new_shipping)
+			
+
 			db.session.commit()
 			return jsonify({'data':'success'})
 		return render_template('shipping.html')
